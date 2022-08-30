f1397cd054020f57b77031efa7c983ce7ea41cf4
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-08-30 22:16:47 +0200

    First version of the router done, basic routes ok

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+
+export default function Contact() {
+  return (
+    <header className="App-header">
+      <Navbar />
+    </header>
+  );
+}
---
+++ frontend/src/pages/FindUs.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+
+export default function FindUs() {
+  return (
+    <header className="App-header">
+      <Navbar />
+    </header>
+  );
+}
